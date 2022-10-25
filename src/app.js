31743d1026a32833157c9da0a7def2a36e9d6374
PedroHBarauna <barauna38@gmail.com>
2022-10-25 14:14:10 -0300

    adicionando o app

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", function(req, res){
+    res.send("Hello World")
+  }); 
+
+app.listen(process.env.PORT || 3333, ()=>{
+    console.log("Aplicação rodando");
+});
+
