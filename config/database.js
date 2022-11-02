b02ec78b97c2ee17c8263d722c0ce156aab7ba71
PedroHBarauna <barauna38@gmail.com>
2022-11-02 14:44:05 -0300

    editando configs da database

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: "postgres",
     host: "localhost",
-    username: "padawanAdm",
-    password: "teste",
-    database: "projetoFramelab",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     define:{
         timestamps: true
     }
